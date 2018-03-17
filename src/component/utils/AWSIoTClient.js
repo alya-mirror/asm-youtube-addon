0e5f198931e18158220ca484768ff3279be6243e
Bilal Al-Saeedi <alronz.bilal@gmail.com>
2018-03-17 21:54:09 +0100

    Small fix

--- src/component/utils/AWSIoTClient.js
+++ src/component/utils/AWSIoTClient.js
@@ -4,13 +4,13 @@ const certsFolderPath = path.resolve('certs');
 
 class AWSIoTClient {
 
-  connect(configs) {
+  connect(configs, clientId) {
     return new Promise((resolve, reject) => {
       this.device = awsIot.device({
         keyPath: `${certsFolderPath}/${configs.keyFileName}`,
         certPath: `${certsFolderPath}/${configs.certFileName}`,
         caPath: `${certsFolderPath}/${configs.caFileName}`,
-        clientId: configs.clientId,
+        clientId: clientId || configs.clientId,
         region: configs.region,
         host: configs.host
       });
